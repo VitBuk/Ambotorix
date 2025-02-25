4a16bd3443da49305b75d7d050b6b72fc4b72b85
VitBuk <vitbuk@gmail.com>
2025-02-25 16:44:08 +0200

    Spring Boot initialized

---
+++ src/test/java/vitbuk/com/Ambotorix/AmbotorixApplicationTests.java
@@ -0,0 +1,13 @@
+package vitbuk.com.Ambotorix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbotorixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
