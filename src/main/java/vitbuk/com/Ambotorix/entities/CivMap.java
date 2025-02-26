f50723a8494c632d5f52ce2674af1fd0f25eaae0
VitBuk <vitbuk@gmail.com>
2025-02-26 17:02:50 +0200

    CivMap enum values now in alphabetical order

--- src/main/java/vitbuk/com/Ambotorix/entities/CivMap.java
+++ src/main/java/vitbuk/com/Ambotorix/entities/CivMap.java
@@ -1,33 +1,33 @@
 package vitbuk.com.Ambotorix.entities;
 
 public enum CivMap {
+    ARCHIPELAGO("Archipelago"),
     CONTINENTS("Continents"),
+    CONTINENTS_AND_ISLANDS("Continents and Islands"),
+    EARTH("Earth"),
+    EARTH_HUGE("Earth Huge"),
+    FOUR_LEAF_CLOVER("4-Leaf Clover"),
     FRACTAL("Fractal"),
+    HIGHLANDS("Highlands"),
     INLAND_SEA("Inland Sea"),
     ISLAND_PLATES("Island Plates"),
     LAKES("Lakes"),
+    MEDITERRANEAN_LARGE("Mediterranean Large"),
+    MIRROR("Mirror"),
     PANGEA("Pangea"),
-    TRUE_START_LOCATION_EARTH_HUGE("True Start Location Earth Huge"),
+    PRIMORDIAL("Primordial"),
+    RICH_HIGHLANDS("Rich Highlands"),
     SEVEN_SEAS("Seven Seas"),
     SHUFFLE("Shuffle"),
-    SMALL_CONTINENTS("Small Continents"),
-    TERRA("Terra"),
-    MEDITERRANEAN_LARGE("Mediterranean Large"),
-    FOUR_LEAF_CLOVER("4-Leaf Clover"),
     SIX_ARMED_SNOWFLAKE("6-Armed Snowflake"),
-    MIRROR("Mirror"),
-    TRUE_START_LOCATION_MEDITERRANEAN("True Start Location Mediterranean"),
-    EARTH("Earth"),
-    EARTH_HUGE("Earth Huge"),
-    ARCHIPELAGO("Archipelago"),
-    HIGHLANDS("Highlands"),
-    WETLANDS("Wetlands"),
-    CONTINENTS_AND_ISLANDS("Continents and Islands"),
-    PRIMORDIAL("Primordial"),
+    SMALL_CONTINENTS("Small Continents"),
     SPLINTERED_FRACTAL("Splintered Fractal"),
+    TERRA("Terra"),
     TILTED_AXIS("Tilted Axis"),
     TILTED_AXIS_WRAPAROUND("Tilted Axis Wraparound"),
-    RICH_HIGHLANDS("Rich Highlands");
+    TRUE_START_LOCATION_EARTH_HUGE("True Start Location Earth Huge"),
+    TRUE_START_LOCATION_MEDITERRANEAN("True Start Location Mediterranean"),
+    WETLANDS("Wetlands");
 
     private final String displayName;
 
