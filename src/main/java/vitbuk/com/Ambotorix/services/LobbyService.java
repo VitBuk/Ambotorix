4029aa02549916b99be5df942ab027eb0c659f5a
VitBuk <vitbuk@gmail.com>
2025-03-20 13:06:35 +0200

    Refactoring: lobby creation in LobbyService is updated

--- src/main/java/vitbuk/com/Ambotorix/services/LobbyService.java
+++ src/main/java/vitbuk/com/Ambotorix/services/LobbyService.java
@@ -16,7 +16,10 @@ public class LobbyService {
             return "Lobby is already exist. " + lobby.getHost().getName() + " can terminate it by using /terminate command";
         } else {
             this.lobby = new Lobby(LocalDateTime.now(), host);
-            return "Lobby is created by " + lobby.getHost().getName();
+            StringBuilder sb = new StringBuilder();
+            sb.append("Lobby is created by ").append(lobby.getHost().getName()).append("\n");
+            // TODO: list of commands host can use after lobby creation
+            return sb.toString();
         }
     }
 }
