d06a8c00bb569971ce4d3721e265e45614902485
VitBuk <vitbuk@gmail.com>
2025-04-14 09:52:01 +0300

    LeadersCommand added

---
+++ src/main/java/vitbuk/com/Ambotorix/commands/LeadersCommand.java
@@ -0,0 +1,17 @@
+package vitbuk.com.Ambotorix.commands;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+import vitbuk.com.Ambotorix.services.AmbotorixService;
+
+public class LeadersCommand implements Command {
+    private static final String commandName =  "/leaders";
+    @Override
+    public String getCommandName() {
+        return commandName;
+    }
+
+    @Override
+    public void execute(Update update, AmbotorixService ambotorixService) {
+        ambotorixService.sendLeaders(update.getMessage().getChatId());
+    }
+}
