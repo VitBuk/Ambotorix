8a6b44c270e56158975a1f3e6bb93e7568527391
VitBuk <vitbuk@gmail.com>
2025-04-24 16:50:33 +0300

    MappoolCommand added

---
+++ src/main/java/vitbuk/com/Ambotorix/commands/MappoolCommand.java
@@ -0,0 +1,20 @@
+package vitbuk.com.Ambotorix.commands;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import vitbuk.com.Ambotorix.commands.structure.Command;
+import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.services.AmbotorixService;
+
+@Component
+public class MappoolCommand implements Command {
+    @Override
+    public String getPrefix() {
+        return CommandConstants.MAPPOLL;
+    }
+
+    @Override
+    public void execute(Update update, AmbotorixService ambotorixService) {
+        ambotorixService.sendMappool(update);
+    }
+}
