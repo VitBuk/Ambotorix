5d3e3e324c65443410a1e4d0a3258d11e94fbb5a
VitBuk <vitbuk@gmail.com>
2025-05-07 17:13:32 +0300

    Refactoring: INFO added to leaders command

--- src/main/java/vitbuk/com/Ambotorix/commands/LeadersCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/LeadersCommand.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class LeadersCommand implements Command {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/leaders",
+            "/leaders",
+            "Show list of all leaders");
     @Override
-    public String getInfo() {
-        return CommandConstants.LEADERS;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
