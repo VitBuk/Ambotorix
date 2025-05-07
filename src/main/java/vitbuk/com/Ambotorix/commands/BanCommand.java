d04fc11cecb12a93289d6b5b397466a2146b7eef
VitBuk <vitbuk@gmail.com>
2025-05-07 11:48:28 +0300

    Command method getInfo() now return ComanndINfo

--- src/main/java/vitbuk/com/Ambotorix/commands/BanCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/BanCommand.java
@@ -3,15 +3,20 @@ package vitbuk.com.Ambotorix.commands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.commands.structure.DynamicCommand;
 import vitbuk.com.Ambotorix.commands.structure.PlayerCommand;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class BanCommand implements DynamicCommand, PlayerCommand {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/time",
+            "/time",
+            "Shows current time in Riga and Munich");
     @Override
-    public String getInfo() {
-        return CommandConstants.BAN;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
