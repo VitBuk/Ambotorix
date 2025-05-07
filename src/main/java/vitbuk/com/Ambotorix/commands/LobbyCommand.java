f56232d47727c6ac99549550686f7e4c09790014
VitBuk <vitbuk@gmail.com>
2025-05-07 17:39:42 +0300

    Refactoring: create game lobby

--- src/main/java/vitbuk/com/Ambotorix/commands/LobbyCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/LobbyCommand.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class LobbyCommand implements Command {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/lobby",
+            "/lobby",
+            "Create game lobby");
     @Override
-    public String getInfo() {
-        return CommandConstants.LOBBY;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
