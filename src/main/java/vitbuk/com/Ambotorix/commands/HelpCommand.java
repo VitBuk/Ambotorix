ac044f058ba934988df595de5e2741757ff7bb83
VitBuk <vitbuk@gmail.com>
2025-05-07 17:09:16 +0300

    Refactoring: INFO added to HelpCommand@

--- src/main/java/vitbuk/com/Ambotorix/commands/HelpCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/HelpCommand.java
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class HelpCommand implements Command {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/help",
+            "/help",
+            "Show list of commands");
     @Override
-    public String getInfo() {
-        return CommandConstants.HELP;
-    }
+    public CommandInfo getInfo() { return INFO; }
 
     @Override
     public void execute(Update update, AmbotorixService ambotorixService) {
