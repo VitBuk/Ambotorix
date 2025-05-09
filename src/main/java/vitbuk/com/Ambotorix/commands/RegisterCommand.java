4e9c54508955e9e105fca224d0b1e8e67da41645
VitBuk <vitbuk@gmail.com>
2025-05-09 12:31:55 +0300

    CommandInfo added to register command

--- src/main/java/vitbuk/com/Ambotorix/commands/RegisterCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/RegisterCommand.java
@@ -4,14 +4,19 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class RegisterCommand implements Command {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/register",
+            "/register",
+            "Register player for current lobby");
 
     @Override
-    public String getInfo() {
-        return CommandConstants.REGISTER;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
