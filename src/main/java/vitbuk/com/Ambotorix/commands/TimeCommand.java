c6c03d44a3a06bd82181942a0f7c9136e47300a0
VitBuk <vitbuk@gmail.com>
2025-05-09 12:37:44 +0300

    CommandINFO added to Time COmmand

--- src/main/java/vitbuk/com/Ambotorix/commands/TimeCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/TimeCommand.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class TimeCommand implements Command {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/time",
+            "/time",
+            "Shows current time for Riga and Munich");
     @Override
-    public String getInfo() {
-        return CommandConstants.TIME;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
