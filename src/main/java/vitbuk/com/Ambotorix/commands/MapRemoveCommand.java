bdcf4cdaed6f7ce7838ba930ab22dfb2765968ba
VitBuk <vitbuk@gmail.com>
2025-05-09 12:14:13 +0300

    CommandINfo added to mapRemove command

--- src/main/java/vitbuk/com/Ambotorix/commands/MapRemoveCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/MapRemoveCommand.java
@@ -3,6 +3,7 @@ package vitbuk.com.Ambotorix.commands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.commands.structure.DynamicCommand;
 import vitbuk.com.Ambotorix.commands.structure.HostCommand;
 import vitbuk.com.Ambotorix.entities.CivMap;
@@ -10,15 +11,19 @@ import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class MapRemoveCommand implements HostCommand, DynamicCommand {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/mapRemove",
+            "/mapRemove",
+            "Removes map from mappool of the current lobby (Host Command)");
     @Override
-    public String getInfo() {
-        return CommandConstants.MAPREMOVE;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
     public void execute(Update update, AmbotorixService ambotorixService) {
         String messageText = update.getMessage().getText().replace("_", "");
-        String mapName = messageText.substring(getInfo().length()).trim();
+        String mapName = messageText.substring(getInfo().prefix().length()).trim();
         CivMap civMap = CivMap.fromDisplayNameIgnoreCase(mapName).get();
 
         ambotorixService.sendMapRemove(update, civMap);
