3c074811620d0df0eb114f50af43e9cc4c83971e
VitBuk <vitbuk@gmail.com>
2025-05-09 12:33:41 +0300

    CommandINFO added to start command

--- src/main/java/vitbuk/com/Ambotorix/commands/StartCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/StartCommand.java
@@ -3,14 +3,19 @@ package vitbuk.com.Ambotorix.commands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.commands.structure.HostCommand;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class StartCommand implements HostCommand {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/start",
+            "/start",
+            "Start generating players order, map and picks for current lobby (Host Command)");
     @Override
-    public String getInfo() {
-        return CommandConstants.START;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
