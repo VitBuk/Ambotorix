4824e3f4783f43b9039b8a4429437f50393d0981
VitBuk <vitbuk@gmail.com>
2025-05-09 12:10:43 +0300

    CommandINFO added to maplist

--- src/main/java/vitbuk/com/Ambotorix/commands/MaplistCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/MaplistCommand.java
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
 import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class MaplistCommand implements Command {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/maplist",
+            "/maplist",
+            "Shows list of all available maps");
     @Override
-    public String getInfo() {
-        return CommandConstants.MAPLIST;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
