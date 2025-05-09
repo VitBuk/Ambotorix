7f6c9e9dcffd70877de4cbecb7a837e41548e6e3
VitBuk <vitbuk@gmail.com>
2025-05-09 12:09:34 +0300

    commandInfo added to mappool command

--- src/main/java/vitbuk/com/Ambotorix/commands/MappoolCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/MappoolCommand.java
@@ -3,14 +3,18 @@ package vitbuk.com.Ambotorix.commands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
-import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
+import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
 @Component
 public class MappoolCommand implements Command {
+    private static final CommandInfo INFO = new CommandInfo(
+            "/mappool",
+            "/mappool",
+            "Shows mappool of the lobby");
     @Override
-    public String getInfo() {
-        return CommandConstants.MAPPOLL;
+    public CommandInfo getInfo() {
+        return INFO;
     }
 
     @Override
