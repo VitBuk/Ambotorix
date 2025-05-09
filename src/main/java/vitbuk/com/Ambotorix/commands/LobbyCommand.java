245d407e396ce3e19d502ee69d90993ded224657
VitBuk <vitbuk@gmail.com>
2025-05-09 12:03:53 +0300

    clearing imports

--- src/main/java/vitbuk/com/Ambotorix/commands/LobbyCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/LobbyCommand.java
@@ -3,7 +3,6 @@ package vitbuk.com.Ambotorix.commands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
-import vitbuk.com.Ambotorix.commands.structure.CommandConstants;
 import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
