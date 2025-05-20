c9ba45f099707dea29aafa747530f898f26e5b4f
VitBuk <vitbuk@gmail.com>
2025-05-20 13:12:21 +0300

    @Component added to SettingsCommand@

--- src/main/java/vitbuk/com/Ambotorix/commands/SettingsCommand.java
+++ src/main/java/vitbuk/com/Ambotorix/commands/SettingsCommand.java
@@ -1,10 +1,12 @@
 package vitbuk.com.Ambotorix.commands;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import vitbuk.com.Ambotorix.commands.structure.Command;
 import vitbuk.com.Ambotorix.commands.structure.CommandInfo;
 import vitbuk.com.Ambotorix.services.AmbotorixService;
 
+@Component
 public class SettingsCommand implements Command {
     private static final CommandInfo INFO = new CommandInfo(
             "/settings",
